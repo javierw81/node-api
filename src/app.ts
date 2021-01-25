3fdc4dbfc29a9df2ad74a7e7170329971a04dc9d
Javier Wamba <javierw81@gmail.com>
2021-01-25 22:59:10 +0100

    refactor: change devenv method

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,8 @@
-import dotenv from 'dotenv';
 import express from 'express';
 import { errorMiddleware } from './middlewares/errorMiddleware';
 import router from './routers/router'
 import * as loggerProvider from './providers/loggerProvider'
 import * as keyValueDatabaseProvider from './providers/keyValueDatabaseProvider'
-
-// load the environment variables from the .env file
-dotenv.config({
-    path: `.env.${process.env.NODE_ENV}`
-})
 
 loggerProvider.config(process.env.LOGGER_LEVEL as string, process.env.LOGGER_FILE as string)
 
--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -1,1 +1,7 @@
+import dotenv from 'dotenv'
+
+export const result = dotenv.config({
+    path: `.env.${process.env.NODE_ENV}`
+})
+
 jest.setTimeout(30000)
